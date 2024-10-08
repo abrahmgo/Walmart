0b1559e1f01f1e4225d00cca7c333d01011bbbc8
Andrés Bonilla <abraham.go@outlook.com>
2024-10-08 17:48:21 -0600

    init repo

---
+++ WExercise/WExercise/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WExercise
+//
+//  Created by Andrés Bonilla Gómez on 08/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
+
